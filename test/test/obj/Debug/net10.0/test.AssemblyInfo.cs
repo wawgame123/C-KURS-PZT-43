c7781674ef33fd3be084d00cc018a00c122f139b
wawgame123 <156342924+wawgame123@users.noreply.github.com>
2026-05-23 01:16:20 +0300

    КУРСЧАЧ
    поправил адаптивку теперь точно все

--- test/test/obj/Debug/net10.0/test.AssemblyInfo.cs
+++ test/test/obj/Debug/net10.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b606656b20a9994f1ae7bb8d2654f7bc3e1c11f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8bcb277d06b3050c0c6ef0fbc6b3f84d635703c")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
